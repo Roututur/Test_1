1c28331ae0837ef5c53468ff9e542aed16760f89
Greg <greg@wdmstudio.fr>
2018-06-18 10:42:09 +0200

    ajout fichier

---
+++ index.php
@@ -0,0 +1,153 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Le portail des JEM</title>
+    <meta name="robots" content="noindex, nofollow">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <meta charset="utf-8" />
+    <meta name="description" content="Le portail des JEM">
+    <meta name="keywords" content="">
+    <meta property="og:title" content="Le portail des JEM" />
+    <meta property="og:type" content="page" />
+    <meta property="og:url" content="" />
+    <meta property="og:image" content="" />
+<link href="https://fonts.googleapis.com/css?family=Jura|Raleway" rel="stylesheet"> 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+
+<div class="container-fluid">
+    <div class="row">
+        <img src="img/bandeau-jem-design.jpg" alt="Portail JEM" class="w-100">
+    </div>
+</div>
+<div class="container-fluid fondNoir">
+    <div class="container">
+        <div class="row py-5">
+            <div class="col-12 col-sm-6 col-lg-3 text-center">
+                              <div class="contImg">
+                <img src="img/jem-export.png" alt="Jem Export">
+                </div>
+                <br>
+                <h2>JEM Export</h2>
+                <p>Négoce de matériaux du bâtiment à l’export </p>
+            </div>
+            <div class="col-12 col-sm-6 col-lg-3 text-center">
+               <div class="contImg">
+                <img src="img/jem-design.png" alt="JEM Design">
+               </div>
+                <br>
+                <h2>JEM Design</h2>
+                <p>Webshop de tables, chaises, produits de décoration</p>
+            </div>
+            <div class="col-12 col-sm-6 col-lg-3 text-center">
+                 <div class="contImg">
+                <img src="img/food.png" alt="JEM FOOD TRADING">
+                </div>
+                <br>
+                <h2>JEM FOOD TRADING</h2>
+                <p> Négoce de viandes surgelées, fruits et légumes et produits laitiers</p>
+            </div>
+            <div class="col-12 col-sm-6 col-lg-3 text-center">
+                              <div class="contImg">
+
+                <img src="img/scavolini-logo.png" alt="JEM Habitat">
+                </div>
+                <br>
+                <h2>JEM HABITAT</h2>
+                <p>Magasin de cuisines, salles de bains, dressing</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid fondBleu">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-5">
+               <div class="row py-5">
+                <div class="col-2">
+                    <img src="img/jem-export-blanc.png" class="w-100" alt="Jem export">
+                </div>
+                <div class="col-10 d-flex align-items-center">
+                    <h3>A propos de JEM</h3>
+                </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 pb-5">
+JEM-EXPORT est une société de négoce créée en 1980.<br>
+Notre expérience et notre savoir-faire nous permettent de proposer des gammes de qualité dans chacune de nos activités.                  
+                 </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-7">
+                <div class="row py-5">
+                    <div class="col-12 col-sm-5 offset-sm-1 order-2 order-sm-1">
+                       <div class="row py-3">
+                        <div class="col-3">
+                            <img src="img/tel.png" alt="Tel Jem Portail">
+                        </div>
+                        <div class="col-8 offset-1">
+                           +33 556 240 606
+                        </div>
+                    </div>
+                       <div class="row py-3">
+                        <div class="col-3">
+                            <img src="img/email.png" alt="Mail Jem Portail" >
+                        </div>
+                        <div class="col-8 offset-1">
+                           info@jem-export.com
+                        </div>
+                    </div>
+                       <div class="row py-3">
+                        <div class="col-3">
+                            <img src="img/location.png" alt="Tel Jem Portail">
+                        </div>
+                        <div class="col-8 offset-1">
+                            Siège social<br>
+18 rue Quintin,<br>
+33000 BORDEAUX
+                        </div>
+                    </div>
+                    </div>
+                    <div class="col-12 col-sm-5 offset-sm-1 order-1 order-sm-2">
+                       <form method="post">
+                        <input placeholder="Prenom" name="prenom" class="w-100 mb-3" required>
+                        <input placeholder="Nom" name="nom" class="w-100 mb-3" required>
+                        <input placeholder="Email" name="email" class="w-100 mb-3" required type="email">
+                        <input placeholder="Societe" name="societe" class="w-100 mb-3">
+                        <textarea name="message" class="w-100 mb-3" placeholder="Message"></textarea>
+                        <button type="submit">Envoyer</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid py-3 footer">
+    <div class="row">
+        <div class="col-12 text-center">
+            © 2018 JEM - <a href="https://wdmstudio.fr">Réalisé par WDM studio</a> - <a href="mentions">Mentions Légales</a>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-12 text-center py-4 petit">
+             <img src="img/footer.png" alt="JEM Export"><br><br>
+             <span class="titre">Union européenne</span><br>
+             Projet bénéficiaire du fond européen <br>
+de développement régional<br>
+             <br>
+             La Région et l’Union Européenne soutiennent le projet « chèque transformation numérique JEM-EXPORT », <br>
+dans le cadre du « programme opérationnel FEDER / FSE 2014-2020.
+        </div>
+    </div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+</body>
+</html>
